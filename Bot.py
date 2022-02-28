d0819552f07f7c63e66fa71bddb5a5de13071d1d
MarKisses <56993916+MarKisses@users.noreply.github.com>
2022-02-28 17:51:29 +0200

    Add files via upload

---
+++ Bot.py
@@ -0,0 +1,19 @@
+import telebot
+import config
+
+bot = telebot.TeleBot(config.TOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def gandon(message):
+    bot.send_message(message.chat.id, 'Пошёл нахуй')
+
+
+@bot.message_handler(content_types=['text'])
+def ohfuck(message):
+    if message.chat.type == "group":
+        bot.reply_to(message, 'Соси хуй и не психуй, дебил')
+
+
+# Run
+bot.polling(none_stop=True)
---
+++ config.py
@@ -0,0 +1,1 @@
+TOKEN = '5237491215:AAGId5V2Ky5A5c6T5aExNVlGDjRbUe-6zKs' # Токен из @BotFather
